dde02256057a51b4333c32b4eb16ea1d32612b48
Liutauras <liutauras.januskas@gmail.com>
2019-09-19 09:46:10 +0300

    * Created board.html file * Created css for board * Created common.css file with `container` and `message` classes styling * Added reset.css * Added `loadClients` function which appends clients to DOM. * Added `_orderClients` function which orders clients

---
+++ assets/js/board.js
@@ -0,0 +1,36 @@
+$(function() {
+    loadClients();
+
+    function _orderClients(clients) {
+        /// Order clients by specialist and number
+        clients.sort(function (a, b) {
+            return a.specialist - b.specialist || a.id - b.id;
+        });
+
+        return clients;
+    }
+
+    function loadClients() {
+        /// Load clients from localStorage
+        var clients = _orderClients(Storage.getItem('clients') || []);
+
+        /// Show message if waiting line is empty
+        if(0 === Object.keys(clients).length) {
+            $("<div class='message'>Empty waiting line</div>").insertBefore(".clients-list");
+            $('.clients-list').hide();
+        } else {
+            $('.clients-list').show();
+            for (var i in clients) {
+                var client = clients[i];
+
+                var template = (
+                    "<li data-id='" + client.id + "'>" +
+                    "<span class='clients-list__specialist'>" + client.specialist + "</span>" +
+                    "<span class='clients-list__number'>" + client.id + "</span>" +
+                    "</li>"
+                );
+                $('.clients-list').append(template);
+            }
+        }
+    }
+});
